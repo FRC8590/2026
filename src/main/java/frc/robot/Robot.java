7f0c4682caf0fe47bbddea89610e54fe09397df7
ConnorGeraghty1 <connoriyg@gmail.com>
2026-02-06 15:22:13 -0500

    Enabled CameraServer.startAutomaticCapture() in Robot.java . Should allow us to put cameras onto smartdashboard/shuffleboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.cameraserver.CameraServer;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as
@@ -30,7 +31,7 @@ public class Robot extends TimedRobot
   {
     instance = this;
 
-    // CameraServer.startAutomaticCapture();
+    CameraServer.startAutomaticCapture();
   }
 
   public static Robot getInstance()
