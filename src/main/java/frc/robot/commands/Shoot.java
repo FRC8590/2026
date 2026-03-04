c310dd93cfb196953af88d57c62cf6030df242d1
decilitre <nlquinn1865@gmail.com>
2026-03-04 16:59:14 -0500

    Peter: Add a docstring.

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -4,6 +4,11 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.commands.ShooterSetSpeed;
 
+/*
+ * Primary command for shooting.
+ * 
+ * This aims and then runs the shooter once aligned.
+ */
 public class Shoot extends SequentialCommandGroup {
     public Shoot() {
         addCommands(
