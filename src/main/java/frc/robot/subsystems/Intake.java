5c94187b0667dd83b1e52ae4be9fe867f3e0ba98
decilitre <nlquinn1865@gmail.com>
2026-03-21 14:24:18 -0400

    Peter and Riley: Adjust intake goal

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -42,16 +42,16 @@ public class Intake extends SubsystemBase {
     /** Relative Encoder */
     private final RelativeEncoder encoder;
     /** PID */
-    private double p = 0.4;
+    private double p = 0.55;
     private double i = 0;
     private double d = 0;
     /** FeedForward */
     private double kv = 0.1;
-    private double kcos = 0.45;
+    private double kcos = 0.5;
     private double kcosratio = 1;
     // also kcos messing things up
     private double goalUpRadians = 0.72;
-    private double goalDownRadians = -0.1;
+    private double goalDownRadians = -0.2;
     private double setPoint = 0.7; // up position is ~0.7, but 0.5 to prevent it trying to go into the hopper,
 
     private ShuffleboardTab tab = Shuffleboard.getTab("Intake");
