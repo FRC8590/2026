debcb275de22c1146b3f9c08da6b582d6641c287
Riley Willis <rileyw.kid@gmail.com>
2026-03-22 10:09:34 -0400

    Riley: Mid. autos now point 10* left at request of Shaheer

--- src/main/java/frc/robot/commands/StableShoot.java
+++ src/main/java/frc/robot/commands/StableShoot.java
@@ -1,15 +1,19 @@
 package frc.robot.commands;
-
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj2.command.*;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 
 import frc.robot.Constants;
 
-public class StableShoot extends SequentialCommandGroup {
+
+public class StableShoot extends ParallelCommandGroup {
     public StableShoot() {
         addCommands(
-                Constants.shooter.shooterSetStableGoalRPM(),
-                new WaitUntilCommand(Constants.shooter::atRPM),
-                Constants.belt.beltAndIndexerRun());
+            Constants.shooter.shooterSetStableGoalRPM(),
+            Constants.belt.beltAndIndexerRun()
+        );
+        addRequirements(getRequirements());
     }
-
+    
 }
