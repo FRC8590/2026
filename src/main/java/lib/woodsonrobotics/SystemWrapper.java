5b71bb14929e5f7c9b0dda5b4521743a975bf69d
Riley Willis <rileyw.kid@gmail.com>
2026-03-31 13:04:58 -0400

    Riley: Made it so SystemWrapper java docs work

--- src/main/java/lib/woodsonrobotics/SystemWrapper.java
+++ src/main/java/lib/woodsonrobotics/SystemWrapper.java
@@ -14,15 +14,20 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-/* Wrapper class around a subsystem.
- * 
- * This allows subsystems to be rebooted or disabled.
-*/
 public class SystemWrapper<T extends SubsystemBase> extends SubsystemBase {
     private final Supplier<T> systemSupplier;
     private T cachedSystem;
     private boolean isEnabled = true;
 
+    /**
+     * TODO: Riley: Peter, you need to add descriptions to these parameters, i dont know what they mean
+     * Wrapper class around a subsystem.
+     * 
+     * This allows subsystems to be rebooted or disabled.
+     * 
+     * @param name
+     * @param supplier
+     */
     public SystemWrapper(String name, Supplier<T> supplier) {
         setName(name);
         this.systemSupplier = supplier;
@@ -40,7 +45,7 @@ public class SystemWrapper<T extends SubsystemBase> extends SubsystemBase {
         }
     }
 
-    /*
+    /**
      * Get the subsystem, or nothing if the system is disabled.
      * 
      * Do not use the resulting subsystem in an addRequirements() call!
@@ -61,7 +66,7 @@ public class SystemWrapper<T extends SubsystemBase> extends SubsystemBase {
         return Optional.of(cachedSystem);
     }
 
-    /*
+    /**
      * Reboot the subsystem. This creates a fresh new instance, which get() will
      * return.
      */
@@ -84,7 +89,9 @@ public class SystemWrapper<T extends SubsystemBase> extends SubsystemBase {
         enable();
     }
 
-    /* Disable the system. */
+    /**
+     * Disable the system.
+     */
     public void disable() {
         System.out.println("Disabled system " + getName());
         isEnabled = false;
@@ -95,7 +102,7 @@ public class SystemWrapper<T extends SubsystemBase> extends SubsystemBase {
         SmartDashboard.putBoolean("Systems/" + getName() + "/Status", false);
     }
 
-    /*
+    /**
      * Enable the system. Systems are enabled by default; you only need to call this
      * if the system was previously disabled by disable(). If disable() wasn't
      * called, this function will effectively do nothing.
@@ -107,7 +114,7 @@ public class SystemWrapper<T extends SubsystemBase> extends SubsystemBase {
         // cachedSystem will be lazily created on next getSystem() call
     }
 
-    /*
+    /**
      * Get a command wrapper for this subsystem.
      * 
      * When the command is invoked, the wrapper will be reinvoked to
@@ -168,7 +175,7 @@ public class SystemWrapper<T extends SubsystemBase> extends SubsystemBase {
         };
     }
 
-    /*
+    /**
      * Wrap a predicate into a BooleanSupplier. This is useful for subsystems
      * that are used with a WaitUntilCommand.
      */
@@ -178,7 +185,7 @@ public class SystemWrapper<T extends SubsystemBase> extends SubsystemBase {
                 .orElse(false); // if disabled, condition is never true
     }
 
-    /* Equivalent to wrapper.get().ifPresent(action) */
+    /** Equivalent to wrapper.get().ifPresent(action) */
     public void ifEnabled(Consumer<? super T> action) {
         var result = get();
         result.ifPresent(action);
